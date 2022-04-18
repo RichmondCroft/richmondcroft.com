e0a69b559a3f44939f41810975ce1a0ea35b997d
Shaurya <88775162+shauryatripaathi@users.noreply.github.com>
2022-04-18 19:31:01 +0530

    Created React  app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import logo from "./assets/images/logo.svg";
-import gearLogo from "./assets/images/gear-logo-image.svg";
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <div className="navigation-bar">
-        <img src={logo} alt="RC" />
-        <div className="navigation-bar__brand-name">Richmond Croft</div>
-      </div>
-      <div className="content">
-        <img className="content__icon" src={gearLogo} alt="Coming soon" />
-        <div className="content__text">
-          Development of the website is in progress..
-        </div>
-      </div>
-    </div>
+   <>
+      
+   </>
   );
 }
 
