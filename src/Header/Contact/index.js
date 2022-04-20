0e9f173c36e5f3a8c8df56c013a5554ea73893da
shaurya_tripathi <shauryatripaathi@gmail.com>
2022-04-20 20:45:54 +0530

    Updated Contact file : Added useState, onChange event ,onSubmit event
    Added useState
    Added name,value attribute to each element
    Added onChange event
    Added onSubmit event

--- src/Header/Contact/index.js
+++ src/Header/Contact/index.js
@@ -1,15 +1,45 @@
+import { useState } from 'react'
 import './style.css'
 
 export default function Contact() {
+    const [input, setInput] = useState({})
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        setInput(values=>({...values,[name]: value}))
+    }
+    const handleOnSubmit = (event)=>{
+        event.preventDefault()
+    }
     return (
         <div className='contact-page-content'>
-            <form className='form-email-text-submit'>
-                <input type='email' id='email-id-box' />
-                <input type='text' id='subject-text-input-box' />
-                <input type='text' id='enter-text-here-input-box' />
-                <input type='submit' 
-                id='confirm-submit-button' 
-                value='Confirm'/>
+            <form className='form-email-text-submit'
+                onSubmit={handleOnSubmit}>
+                <input type='email'
+                    name='emailId'
+                    value={input.emailId || ''}
+                    onChange={handleChange}
+                    id='email-id-box'
+                />
+
+                <input type='text'
+                    name = 'subject'
+                    value={input.subject || '' }
+                    onChange={handleChange}
+                    id='subject-text-input-box'
+                />
+
+                <input type='text'
+                    name = 'enterText'
+                    value={input.enterText || ''}
+                    onChange={handleChange}
+                    id='enter-text-here-input-box'
+                />
+
+                <input type='submit'
+                    id='confirm-submit-button'
+                    value='Confirm'
+                />
             </form>
         </div>
     )
