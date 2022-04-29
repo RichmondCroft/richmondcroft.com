ffed91453f25a83be34fd2cf90f5846d6bc2ab96
shaurya_tripathi <shauryatripaathi@gmail.com>
2022-04-29 19:28:10 +0530

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -11,18 +11,10 @@ export default function Layout() {
                         <img src={RCLogo} alt='RC-logo' width='50px' />
                         Richmond Croft</div>
                     <ul className="navigation-menu-components">
-                        <li>
-                            <Link to='/' className="navigation-menu-tab-links">Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/about' className="navigation-menu-tab-links">About</Link>
-                        </li>
-                        <li>
-                            <Link to='/project' className="navigation-menu-tab-links">Projects</Link>
-                        </li>
-                        <li>
-                            <Link to='/contact' className="navigation-menu-tab-links">Contact Us</Link>
-                        </li>
+                        <li><Link to='/' className="navigation-menu-tab-links">Home</Link></li>
+                        <li><Link to='/about' className="navigation-menu-tab-links">About</Link></li>
+                        <li><Link to='/project' className="navigation-menu-tab-links">Projects</Link></li>
+                        <li><Link to='/contact' className="navigation-menu-tab-links">Contact Us</Link></li>
                     </ul>
                 </nav>
             </div>
