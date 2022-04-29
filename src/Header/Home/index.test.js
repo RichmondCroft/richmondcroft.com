2290f6d343fe78c4b0579d0179096c4261efb569
shaurya_tripathi <shauryatripaathi@gmail.com>
2022-04-29 17:37:57 +0530

    Update index.test.js

--- src/Header/Home/index.test.js
+++ src/Header/Home/index.test.js
@@ -1,15 +1,15 @@
-import { render,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "./index";
 
-test('should test heading',()=>{
+test('should test heading', () => {
     render(<Home />);
     const homeElement = screen.getAllByRole('heading');
     expect(homeElement.length).toBe(1)
 })
 
-test('should test heading description',()=>{
+test('should test heading description', () => {
     render(<Home />);
-    const homeElement  = screen.getByText(/Welcome/i)
+    const homeElement = screen.getByText(/Welcome/i)
     expect(homeElement).toBeInTheDocument()
 })
 
